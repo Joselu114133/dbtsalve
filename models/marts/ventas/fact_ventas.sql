ae46accfaaf346d7c797b664702726a891ead794
Jose Luis Quesada Pérez <heiterman0501@gmail.com>
2025-05-21 17:07:23 +0000

    incrementales

--- models/marts/ventas/fact_ventas.sql
+++ models/marts/ventas/fact_ventas.sql
@@ -1,15 +1,24 @@
 
-{{ config(materialized='table') }}
-{{ config(database='ALUMNO14_DEV_GOLD_DB') }}
-{{ config(schema='ventas') }}
+-- {{ config(materialized='table') }}
+{{ config(database='ALUMNO14_DEV_GOLD_DB' ,
+    materialized='incremental',
+    unique_key='id_venta',
+  schema='ventas'  ) }}
+
 
 SELECT
-    id_venta,        
-    id_cliente,     
-    id_vehiculo,     
-    id_vendedor,     
-    id_ciudad as id_sucursal,       --
-    fecha_venta,
-    precio_final,
-    metodo_pago
-FROM {{ ref('stg_concesionario__ventas') }}
+    v.id_venta,
+    v.fecha_venta,
+    v.id_cliente,
+    v.id_ciudad as id_sucursal,
+    v.id_vendedor,
+    v.id_vehiculo,
+    v.metodo_pago,
+    v.precio_final
+FROM
+    {{ ref('stg_concesionario__ventas') }} v
+
+{% if is_incremental() %}
+    -- Solo inserta las ventas nuevas (las que no existen aún en la tabla incremental)
+    WHERE v.fecha_venta >  (select max(fecha_venta) from {{ this }})
+{% endif %}
